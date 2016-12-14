8df9d1c9dec5f1cd9b22d462e787bc4eebc9bb98
Brago <bragolab@gmail.com>
2016-12-14 12:41:28 +0100

    Created CardOne project.

---
+++ CardOne/Assets/Scripts/GamePlayManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GamePlayManager : MonoBehaviour {
+
+    void Start() {
+
+        SetUpPlayers();
+        SetUpBoard();
+        SetUpCards();
+        SetUpRound();
+    }
+
+    /// <summary>
+    /// Setta i players
+    /// </summary>
+	void SetUpPlayers() {
+        Debug.Log("Setup Players");
+    }
+    /// <summary>
+    /// Setta il tavolo
+    /// </summary>
+    void SetUpBoard() {
+        Debug.Log("Setup Board");
+    }
+    /// <summary>
+    /// Setta le carte
+    /// </summary>
+    void SetUpCards() {
+        Debug.Log("Setup Cards");
+    }
+    /// <summary>
+    /// Setta il round
+    /// </summary>
+    void SetUpRound() {
+        Debug.Log("Setup Round");
+    }
+}
