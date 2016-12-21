b85bd282d0849438cef6e147fdbc2071d395f429
Brago <bragolab@gmail.com>
2016-12-21 12:48:51 +0100

    - CardDataManager e struttura dati per card con scriptableObject.

--- CardOne/Assets/Scripts/GamePlayManager.cs
+++ CardOne/Assets/Scripts/GamePlayManager.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 
 public class GamePlayManager : MonoBehaviour {
 
-
 	public static GamePlayManager gpm;
 	public int CurrentRound ;
 	Player currentPLayer;
@@ -18,17 +17,17 @@ public class GamePlayManager : MonoBehaviour {
 	}
 
     void Start() {
-
-        SetUpPlayers();
-        SetUpBoard();
-        SetUpCards();//
-        SetUpRound();
+        GameLevel levelToLoad = GetLevelInfo(2);
+        SetUpPlayers(levelToLoad);
+        SetUpBoard(levelToLoad);
+        SetUpCards(levelToLoad);
+        SetUpRound(levelToLoad);
     }
 
     /// <summary>
     /// Setta i players
     /// </summary>
-	void SetUpPlayers() {
+	void SetUpPlayers(GameLevel _gameLeveldata) {
         Debug.Log("Setup Players");
 		//foreach (Player p in players) {
 			//currentPLayer.Life = 20;
@@ -39,25 +38,42 @@ public class GamePlayManager : MonoBehaviour {
     /// <summary>
     /// Setta il tavolo
     /// </summary>
-    void SetUpBoard() {
+    void SetUpBoard(GameLevel _gameLeveldata) {
         Debug.Log("Setup Board");
+        foreach (BoardCol col in _gameLeveldata.Cols) {
+            switch (col.ColType) {
+                case 0:
+                    // base
+                    break;
+                case 1:
+                    // Acqua
+                    break;
+                case 2:
+                    // Sopraelevato
+                    break;
+                default:
+                    // Non utilizzato
+                    break;
+            }
+        }
     }
     /// <summary>
     /// Setta le carte
     /// </summary>
-    void SetUpCards() {
-        Debug.Log("Setup Cards");
+    void SetUpCards(GameLevel _gameLeveldata) {
+        Debug.LogFormat("Setup Cards {0}", _gameLeveldata.AllCards.Count);
+
     }
     /// <summary>
     /// Setta il round
     /// </summary>
-    void SetUpRound() {
+    void SetUpRound(GameLevel _gameLeveldata) {
         Debug.Log("Setup Round");
 		CurrentRound++;
 		currentPLayer = RandomPlayer ();
 		currentPLayer.MyTurn = true;
 		if (CurrentRound == 1) {
-			DealCards (4);
+			DealCards (_gameLeveldata.StartNumberOfCards);
 		} 
 		else {
 			DealCards (1);
@@ -93,6 +109,85 @@ public class GamePlayManager : MonoBehaviour {
 	void DealCards (int numberOfCards){
 		//dai carte
 	}
+
+
+    int GetInitialCardNumberFromDifficultyLevel(int _levelId) {
+        switch (_levelId) {
+            case 1:
+                return 5;
+            case 2:
+                return 4;
+            case 3:
+                return 3;
+        }
+
+        return 0;
+    }
+
+
+    GameLevel GetLevelInfo(int _levelId) {
+        GameLevel returnGameLevel = new GameLevel();
+        switch (_levelId) {
+            case 1:
+                returnGameLevel = new GameLevel() {
+                    StartNumberOfCards = 5,
+                    Cols = new List<BoardCol>() {
+                                             new BoardCol() { ColType = -1 },
+                                             new BoardCol() { ColType = 0 },
+                                             new BoardCol() { ColType = 0 },
+                                             new BoardCol() { ColType = 0 },
+                                             new BoardCol() { ColType = -1 },
+                                         }
+                };
+                break;
+            case 2:
+                returnGameLevel = new GameLevel() {
+                    StartNumberOfCards = 4,
+                    Cols = new List<BoardCol>() {
+                                             new BoardCol() { ColType = 0 },
+                                             new BoardCol() { ColType = 0 },
+                                             new BoardCol() { ColType = 0 },
+                                             new BoardCol() { ColType = 0 },
+                                             new BoardCol() { ColType = 0 },
+                                          }
+                };
+                break;
+            case 3:
+                returnGameLevel = new GameLevel() {
+                    StartNumberOfCards = 2,
+                    Cols = new List<BoardCol>() {
+                                             new BoardCol() { ColType = 1 },
+                                             new BoardCol() { ColType = 0 },
+                                             new BoardCol() { ColType = 0 },
+                                             new BoardCol() { ColType = 0 },
+                                             new BoardCol() { ColType = 2 },
+                                         },
+                    ThemePrefix = "Disco",
+                };
+                break;
+            default:
+                returnGameLevel = new GameLevel();
+                break;
+        }
+
+        returnGameLevel.AllCards = CardManager.GetAllCards();
+
+        return returnGameLevel;
+    }
+
+
+    #region Data Structure for Game Level 
+    public class GameLevel {
+        public int StartNumberOfCards;
+        public List<BoardCol> Cols;
+        public string ThemePrefix;
+        public List<CardData> AllCards;
+    }
+
+    public class BoardCol {
+        public int ColType;
+    }
+    #endregion
 
 
 }
