56c30b7b7f07f7e709be37760aae774db1021393
Marco Conio <marco.conio@gmail.com>
2016-12-21 01:07:21 +0100

    Aggiunta attributi e costruttore carte

--- CardOne/Assets/Scripts/Board.cs
+++ CardOne/Assets/Scripts/Board.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Board : MonoBehaviour {
 
-	public Card.Slot TypeOfTerrain = Card.Slot.MyTable;
+//	public Card.Slot TypeOfTerrain = Card.Slot.MyTable;
 
 
 }
