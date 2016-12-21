cafabb2a1b3c67cf316f16887c93bbfc5484fe35
Brago <bragolab@gmail.com>
2016-12-21 01:23:59 +0100

    - ScriptableObject example.

---
+++ EH_SoftwareDev_2_Learn/Assets/ScriptableObjects/Scripts/LevelInfo.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "LevelInfoData", menuName = "Level/LevelInfo", order = 1)]
+public class LevelInfo : ScriptableObject {
+
+    public string Name;
+    public LevelTypes LevelType;
+    public Color LevelColor;
+
+    public enum LevelTypes { Type1, Type2, }
+}
---
+++ EH_SoftwareDev_2_Learn/Assets/ScriptableObjects/Scripts/LevelSetupFromData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class LevelSetupFromData : MonoBehaviour {
+
+    public List<LevelInfo> LevelsData;
+
+	// Use this for initialization
+	void Start () {
+        foreach (LevelInfo levelData in LevelsData) {
+            Debug.LogFormat("Level {0}", levelData.Name);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
