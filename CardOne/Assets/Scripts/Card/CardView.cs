f1f73521a4386b4939c9c5130b2d070afd996b80
Brago <brago@indieconstruction.com>
2016-12-22 13:00:28 +0100

    - creata cardView.

---
+++ CardOne/Assets/Scripts/Card/CardView.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardView : MonoBehaviour {
+
+    public CardData Data;
+
+    /// <summary>
+    /// Init dei dati.
+    /// </summary>
+    /// <param name="_cardData"></param>
+    public void Init(CardData _cardData) {
+        Data = _cardData;
+        InitGraphic();
+    }
+
+    /// <summary>
+    /// Fa tutte le cose che servono a visualizzare i dati nella carta.
+    /// </summary>
+    public void InitGraphic() {
+        /// TODO: CardView InitGraphic
+        /// - Visualizzare la label con l'id della carta
+        /// - Visualizzare la life
+        /// - Attack
+        /// ...
+        
+    }
+}
+
+
