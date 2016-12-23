659afbef93eba11443f1bb6a90eabc1461668a5a
Brago <bragolab@gmail.com>
2016-12-23 01:53:44 +0100

    - Fix errori riferimento a Card Oject.

--- CardOne/Assets/Scripts/CardsList.cs
+++ CardOne/Assets/Scripts/CardsList.cs
@@ -3,5 +3,5 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class CardsList : MonoBehaviour {
-	public List <Card> CardPack = new List<Card> ();
+	//public List <Card> CardPack = new List<Card> ();
 }
--- CardOne/Assets/Scripts/Deck.cs
+++ CardOne/Assets/Scripts/Deck.cs
@@ -7,12 +7,12 @@ public class Deck : MonoBehaviour {
 	public static void DealCards(int numberOfCards, Player currentPLayer)
 	{
 		
-		List<Card> CurrentPack = new List<Card>();
-		CurrentPack = currentPLayer.MyDeck.CardPack;
-		for (int i = 0; i < numberOfCards; i++) {
-			int rand = Random.Range (0, CurrentPack.Count);
-			Debug.Log (CurrentPack [rand]);
-			CurrentPack.Remove (CurrentPack [rand]);
-		}
+		//List<Card> CurrentPack = new List<Card>();
+		//CurrentPack = currentPLayer.MyDeck.CardPack;
+		//for (int i = 0; i < numberOfCards; i++) {
+		//	int rand = Random.Range (0, CurrentPack.Count);
+		//	Debug.Log (CurrentPack [rand]);
+		//	CurrentPack.Remove (CurrentPack [rand]);
+		//}
 	}
 }
